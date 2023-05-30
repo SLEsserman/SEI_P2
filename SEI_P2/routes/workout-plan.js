b586b9e0e2f4c5ea715e98e8c8681fd0017a7d8b
Sam Esserman <sesserman97@gmail.com>
2023-05-30 16:06:38 -0400

    mvp almost done

--- SEI_P2/routes/workout-plan.js
+++ SEI_P2/routes/workout-plan.js
@@ -11,11 +11,42 @@ router.get("/", ensureLoggedIn, function (req, res, next) {
   })
 })
 
+router.post("/", ensureLoggedIn, async function (req, res) {
+  // Render the "workoutplan/new" view with the title "Create Workout plans"
+
+  let workoutPlan = await WorkoutPlan.create(req.body)
+  console.log("req body", req.body)
+  console.log("plan", workoutPlan)
+  res.redirect("/workout-plan/" + workoutPlan._id)
+})
+
 router.get("/new", ensureLoggedIn, function (req, res, next) {
   // Render the "workoutplan/new" view with the title "Create Workout plans"
   res.render("workoutplan/new", {
     title: "Create Workout plans",
   })
+})
+
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  // Render the "workoutplan/new" view with the title "Create Workout plans"
+  let planId = req.params.id
+  let plan = await WorkoutPlan.findById(planId)
+  console.log("plan", plan)
+  res.render("workoutplan/update", {
+    title: "Add Exercise to Workout plans",
+    plan,
+  })
+})
+
+router.put("/:id/exercise", ensureLoggedIn, async function (req, res) {
+  // Render the "workoutplan/new" view with the title "Create Workout plans"
+  let planId = req.params.id
+
+  let workoutPlan = await WorkoutPlan.findById(planId)
+  workoutPlan.exercises = [...workoutPlan.exercises, req.body]
+  await workoutPlan.save()
+
+  res.redirect("/workout-plan/" + workoutPlan._id)
 })
 
 module.exports = router
